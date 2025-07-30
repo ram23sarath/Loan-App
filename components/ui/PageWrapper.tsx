000a6e1b834b656db1dbce3689a83e773448d35f
ram23sarath <38622885+ram23sarath@users.noreply.github.com>
2025-07-30 17:07:36 +0530

    Add files via upload

---
+++ components/ui/PageWrapper.tsx
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import { motion, Transition } from 'framer-motion';
+
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    y: 20,
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+  },
+  out: {
+    opacity: 0,
+    y: -20,
+  },
+};
+
+const pageTransition: Transition = {
+  type: 'tween',
+  ease: 'anticipate',
+  duration: 0.5,
+};
+
+const PageWrapper = ({ children }: { children: React.ReactNode }) => (
+  <motion.div
+    initial="initial"
+    animate="in"
+    exit="out"
+    variants={pageVariants}
+    transition={pageTransition}
+    className="w-full h-full p-8"
+  >
+    {children}
+  </motion.div>
+);
+
+export default PageWrapper;

7ebd2a8f9026de3e4f05f0518695e1d7aeeebfa4
Jaime <ram23sarath@gmail.com>
2025-09-26 10:44:57 +0530

    Added subtypes in the Misc Entries

--- constants.tsx
+++ constants.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
+
 // BookOpenIcon for summary (Heroicons outline)
 export const BookOpenIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
+    xmlns="http://www.w3.org/2000/svg" // Added missing xmlns attribute
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
@@ -12,8 +15,6 @@ export const BookOpenIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <path d="M2.25 6.75C2.25 5.507 3.257 4.5 4.5 4.5h5.25v15H4.5a2.25 2.25 0 01-2.25-2.25v-12zM21.75 6.75C21.75 5.507 20.743 4.5 19.5 4.5h-5.25v15h5.25a2.25 2.25 0 002.25-2.25v-12z" />
   </svg>
 );
-
-import React from 'react';
 
 export const UserPlusIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><line x1="19" x2="19" y1="8" y2="14"/><line x1="22" x2="16" y1="11" y2="11"/></svg>
@@ -36,7 +37,12 @@ export const HistoryIcon = (props: React.SVGProps<SVGSVGElement>) => (
 );
 
 export const Trash2Icon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M3 6h18"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/><line x1="10" y1="11" x2="10" y2="17"/><line x1="14" y1="11" x2="14" y2="17"/></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M3 6h18"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/><line x1="10" y1="11" x2="10" y2="17"/><line x1="14" y1="11" x2="14" y2="17"/></svg>
+);
+export const PencilIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M15.232 5.232a2.5 2.5 0 0 1 3.536 3.536l-9.5 9.5a2 2 0 0 1-.878.513l-4 1a1 1 0 0 1-1.213-1.213l1-4a2 2 0 0 1 .513-.878l9.5-9.5z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
 );
 
 export const FileDownIcon = (props: React.SVGProps<SVGSVGElement>) => (
@@ -49,7 +55,8 @@ export const XIcon = (props: React.SVGProps<SVGSVGElement>) => (
 
 export const WhatsAppIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" {...props}>
-        <path d="M12 2.04C6.5 2.04 2 6.53 2 12c0 1.74.45 3.38 1.22 4.82L2 22l5.3-1.48C8.68 21.43 10.3 22 12 22c5.5 0 10-4.47 10-9.96S17.5 2.04 12 2.04zm4.84 11.4-1.12.56c-.15.07-.3.12-.45.1-.22-.03-.43-.1-.6-.23-.82-.4-1.6-1-2.28-1.7-1.02-1.05-1.7-2.2-1.95-3.57-.05-.22.05-.42.2-.6.15-.17.32-.25.5-.25.08 0 .16 0 .23.02.18.05.35.1.5.15l1.1.45c.15.07.28.15.4.25.12.1.2.22.25.35.05.13.07.27.02.4-.05.13-.12.25-.22.35l-.28.33c-.08.1-.12.2-.12.3s.03.2.1.32c.2.35.5.72.9 1.1.45.42.9.75 1.35.98.15.08.3.13.45.13.2 0 .37-.07.5-.2.13-.13.2-.27.23-.42.03-.15.03-.3 0-.43l-.38-1.02c-.07-.2-.05-.4.05-.58.1-.18.25-.32.45-.42l1.1-.53c.2-.1.4-.12.6-.08.2.04.4.15.55.3.15.17.22.35.22.55s-.05.4-.15.58l-1.1 1.05c-.1.1-.2.2-.3.3z"></path>
+        <path d="M12 2.04C6.5 2.04 2 6.53 2 12c0 1.74.45 3.38 1.22 4.82L2 22l5.3-1.48C8.68 21.43 10.3 22 12 22c5.5 0 10-4.47 10-9.96S17.5 2.04 12 2.04zm4.84 11.4-1.12.56c-.15.07-.3.12-.45.1-.22-.03-.43-.1-.6-.23-.82-.4-1.6-1-2.28-1.7-1.02-1.05-1.7-2.2-1.95-3.57-.05-.22.05-.42.2-.6.15-.17.32-.25.5-.25.08 0 .16 0 .23.02.18.05.35.1.5.15l1.1.45c.15.07.28.15.4.25.12.1.2.22.25.35.05.13.07.27.02.4-.05.13-.12.25-.22.35l-.28.33c-.08.1-.12.2-.12.3s.03.2.1.32c.2.35.5.72.9 1.1.45.42.9.75 1.35.98.15.08.3.13.45.13.2 0 .37-.07.5-.2.13-.13.2-.27.23-.42.03-.15.03-.3 0-.43l-.38-1.02c-.07-.2-.05-.4.05-.58.1-.18.25-.32.45-.42l1.1-.53c.2-.1.4-.12.6-.08.2.04.4.15.55.3.15.17.22.35.22.55s-.05.4-.15.58l-1.1 1.05c-.1.1-.2.2-.3.3z" /> 
+        {/* Changed <path></path> to self-closing <path /> for JSX consistency */}
     </svg>
 );
 

58ded2e6fb6d784997cf0065ae6cc2bae6a05683
ram23sarath <38622885+ram23sarath@users.noreply.github.com>
2025-11-22 10:06:05 +0000

    Added new change password option

--- constants.tsx
+++ constants.tsx
@@ -75,3 +75,7 @@ export const SpinnerIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <path d="M21 12a9 9 0 1 1-6.219-8.56"/>
   </svg>
 );
+
+export const KeyIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M21 2l-2 2m-5.5-2a6 6 0 1 0 0 12 6 6 0 0 0 0-12z"/><path d="M16 8h5"/></svg>
+);
